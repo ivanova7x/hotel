42b820962179fbb5a37c3d34ef7a708374a40ae7
Tsvetelina D. Ivanova <tsvetelina.ivanova23@gmail.com>
2018-03-03 07:47:32 +0200

    changed gallery page

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,16 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a "loop" variable
+var i;
+
+// Two images side by side
+function two() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "50%";
+    }
+}
