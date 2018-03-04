8af8076032a62fc9c920af99b204d22a159e3c46
Tsvetelina D. Ivanova <tsvetelina.ivanova23@gmail.com>
2018-03-04 04:18:14 +0200

    responsive header

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ $(document).ready(function() {
         $('div#modal img').attr('src', $(this).attr('data-image-url') );
     });
 });
+
+/* Toggle between adding and removing the "responsive" class to navigation when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myNavigation");
+    if (x.className === "navigation") {
+        x.className += " responsive";
+    } else {
+        x.className = "navigation";
+    }
+}
